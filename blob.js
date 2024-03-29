4737b97832cefd3f895b81d8297a9e813f2d1ff9
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-29 15:32:24 +0000

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,14 +1,13 @@
-/// blob.js
 document.addEventListener("keydown", function (blob) {
-	if (blob.key == "~" && blob.ctrlKey) {
-		var win = window.open("", "_blank", "width=500,height=300");
-		var blob = win.document.createElement("iframe");
-		(blob.src = "https://blobby-boi.github.io/BlobeBMBypass/main.html"),
-			(blob.style.cssText = "width:100%; height:100%; border:none;"),
-			win.document.body.appendChild(blob),
-			win.document.title = "BlobeBM",
-			win.addEventListener("message", function (blob) {
-				blob.data.toString().startsWith("execute:") && (eval(blob.data.toString().replace("execute:", "")), win.close());
-			});
+if (blob.key == "~" && blob.ctrlKey) {
+	var win = window.open("", "_blank", "width=500,height=300");
+	var blob = win.document.createElement("iframe");
+	(blob.src = "https://blobby-boi.github.io/BlobeBMBypass/main.html"),
+	(blob.style.cssText = "width:100%; height:100%; border:none;"),
+	win.document.body.appendChild(blob),
+	win.document.title = "BlobeBM",
+	win.addEventListener("message", function (blob) {
+		blob.data.toString().startsWith("execute:") && (eval(blob.data.toString().replace("execute:", "")), win.close());
+		});
 	}
 });
