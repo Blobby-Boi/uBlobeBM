68b162ff1336265b26452e7457b3138502aabbdc
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-29 15:31:33 +0000

    Update blob.js

--- blob.js
+++ blob.js
@@ -6,7 +6,7 @@ document.addEventListener("keydown", function (blob) {
 		(blob.src = "https://blobby-boi.github.io/BlobeBMBypass/main.html"),
 			(blob.style.cssText = "width:100%; height:100%; border:none;"),
 			win.document.body.appendChild(blob),
-			win.document.title = "uRun",
+			win.document.title = "BlobeBM",
 			win.addEventListener("message", function (blob) {
 				blob.data.toString().startsWith("execute:") && (eval(blob.data.toString().replace("execute:", "")), win.close());
 			});
