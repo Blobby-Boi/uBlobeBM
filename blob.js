ed6492a4db6e416119b44b09252b64c085d92f80
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-03-30 09:06:23 -0700

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,7 +1,7 @@
 /// blob.js
 document.addEventListener("keydown", function (blob) {
 if (blob.key == "~" && blob.ctrlKey) {
-	var win = window.open("", "_blank", "width=800,height=600,top=100,left=100,fullscreen=yes");
+	var win = window.open("", "_blank", "width=1300,height=600,top=100,left=100");
 	var blob = win.document.createElement("iframe");
 	(blob.src = "https://blobby-boi.github.io/uBlobeBM/main.html"),
 	(blob.style.cssText = "width:100%; height:100%; border:none;"),
