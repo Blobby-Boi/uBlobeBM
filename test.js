f327971dad8d981fd4c384c6df20ec8ddd7a022a
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-05-31 08:27:30 -0700

    Update and rename test.js to test2.js

--- test.js
+++
@@ -1,4 +0,0 @@
-/// test.js
-var script = document.createElement('script');
-script.src = 'https://blobby-boi.github.io/uBlobeBM/blob.js';
-document.body.appendChild(script);
---
+++ test2.js
@@ -0,0 +1,6 @@
+/// test2.js
+document.addEventListener('DOMContentLoaded', function() {
+    var script = document.createElement('script');
+    script.src = 'https://blobby-boi.github.io/uBlobeBM/blob.js';
+    document.body.appendChild(script);
+});
