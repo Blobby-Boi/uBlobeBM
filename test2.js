52be92464cda7dc3a28db804ca88613932f00ae4
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-05-31 08:29:33 -0700

    Delete test2.js

--- test2.js
+++
@@ -1,6 +0,0 @@
-/// test2.js
-document.addEventListener('DOMContentLoaded', function() {
-    var script = document.createElement('script');
-    script.src = 'https://blobby-boi.github.io/uBlobeBM/blob.js';
-    document.body.appendChild(script);
-});
