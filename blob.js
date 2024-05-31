394227edf8e848cff764a7d0e01d5f123996a103
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-05-30 21:18:22 -0700

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,22 +1,18 @@
-document.addEventListener("keydown", function (event) {
-  if (event.key == "~" && event.ctrlKey) {
-    var win = window.open("", "_blank", "width=1250,height=600,top=100,left=50");
-    var blob = win.document.createElement("iframe");
-    blob.src = "https://blobby-boi.github.io/uBlobeBM/main.html";
-    blob.style.cssText = "width:100%; height:100%; border:none;";
-    win.document.body.appendChild(blob);
-    win.document.title = "BlobeBM";
-    var faviconLink = win.document.createElement("link");
-    faviconLink.rel = "shortcut icon";
-    faviconLink.href = "https://blobby-boi.github.io/uBlobeBM/favicon.png";
-    win.document.head.appendChild(faviconLink);
-    win.addEventListener("message", function (messageEvent) {
-      if (messageEvent.data.toString().startsWith("run:")) {
-        const codeToRun = messageEvent.data.toString().replace("run:", "");
-        const func = new Function(codeToRun);
-        func();
-        win.close();
-      }
-    });
-  }
+/// blob.js
+document.addEventListener("keydown", function (blob) {
+if (blob.key == "~" && blob.ctrlKey) {
+	var win = window.open("", "_blank", "width=1250,height=600,top=100,left=50");
+	var blob = win.document.createElement("iframe");
+	(blob.src = "https://blobby-boi.github.io/uBlobeBM/main.html"),
+	(blob.style.cssText = "width:100%; height:100%; border:none;"),
+	win.document.body.appendChild(blob),
+	win.document.title = "BlobeBM";
+	var faviconLink = win.document.createElement("link");
+        faviconLink.rel = "shortcut icon";
+        faviconLink.href = "https://blobby-boi.github.io/uBlobeBM/favicon.png";
+	win.document.head.appendChild(faviconLink);
+	win.addEventListener("message", function (blob) {
+		blob.data.toString().startsWith("run:") && (eval(blob.data.toString().replace("run:", "")), win.close());
+		});
+	}
 });
