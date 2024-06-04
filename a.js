e262aa36d18627875744721397aea246b6e90601
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-06-03 21:58:32 -0700

    Update and rename e.js to a.js

---
+++ a.js
@@ -0,0 +1,3 @@
+/// a.js
+
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/Blobby-Boi/BOOKMARKLET-GAMES@main/V5.0BreakoutBookmarklet.js';})();
