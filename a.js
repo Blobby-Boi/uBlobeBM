af25ccb17e5e2a2df53f0134e24f0a0da0d39494
BlobbyBoi <131911782+Blobby-Boi@users.noreply.github.com>
2024-06-03 22:02:11 -0700

    Delete a.js

--- a.js
+++
@@ -1,3 +0,0 @@
-/// a.js
-
-javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/Blobby-Boi/BOOKMARKLET-GAMES@main/V5.0BreakoutBookmarklet.js';})();
