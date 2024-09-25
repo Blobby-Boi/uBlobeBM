ac51e8cb3e1e6d3be9fc9f21bd005dd7144419d9
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2024-09-25 10:35:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -155,6 +155,9 @@ function handleMessage(message) {
         blobFrame = null;
 
         setTimeout(() => {
+            try {
+                eval(decodeURIComponent(message.data.toString().replace("run:", "")));
+            } catch (error) {
             let messageData = message.data.toString().replace("run:", "");
             const replacements = {
                 '%20': ' ',
@@ -196,7 +199,8 @@ function handleMessage(message) {
             } catch (error) {
                     console.error('Error executing bookmarklet:', error.message);
                     alert('An error occured while executing the bookmarklet. Try double checking the code of the bookmarklet. Error: ' + error.message);
-            }
+              }
+           }
         }, 200);
     }
 }
