4d18058aa6e41ab20a1dc83d2cbcbac8bfcb06e6
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-03 12:29:25 -0800

    use raw instead of GitHub pages

--- blob.js
+++ blob.js
@@ -1,6 +1,6 @@
 /// blob.js
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.href !== 'https://blobby-boi.github.io/uBlobeBM/main.html') {
+    if (window.location.href !== 'https://github.com/Blobby-Boi/uBlobeBM/raw/refs/heads/main/main.js') {
         var script = document.createElement('script');
         script.src = 'https://blobby-boi.github.io/uBlobeBM/main.js';
         document.body.appendChild(script);
