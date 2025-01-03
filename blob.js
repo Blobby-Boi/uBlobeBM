5cff1745116b4b07d7241e6cd36ab48881112c17
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-03 12:30:18 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,8 +1,8 @@
 /// blob.js
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.href !== 'https://github.com/Blobby-Boi/uBlobeBM/raw/refs/heads/main/main.js') {
+    if (window.location.href !== 'https://blobby-boi.github.io/uBlobeBM/main.html') {
         var script = document.createElement('script');
-        script.src = 'https://blobby-boi.github.io/uBlobeBM/main.js';
+        script.src = 'https://github.com/Blobby-Boi/uBlobeBM/raw/refs/heads/main/main.js';
         document.body.appendChild(script);
     }
 });
