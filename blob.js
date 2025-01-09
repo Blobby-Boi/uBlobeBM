657c7a8552e2629217c07e97f0bafd7d317578e1
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-09 13:19:50 -0800

    simplification

--- blob.js
+++ blob.js
@@ -1,20 +1,13 @@
 /// blob.js
-document.addEventListener('DOMContentLoaded', function() {
-    var script = document.createElement('script');
+document.addEventListener('DOMContentLoaded', function () {
     var main = 'https://cdn.jsdelivr.net/gh/Blobby-Boi/uBlobeBM@main/main.js';
     var fallback = 'https://blobby-boi.github.io/uBlobeBM/main.js';
-    
-    fetch(main, { method: 'HEAD' })
-        .then(function(response) {
-            if (response.ok) {
-                script.src = main;
-            } else {
-                script.src = fallback;
-            }
-            document.body.appendChild(script);
-        })
-        .catch(function() {
-            script.src = fallback;
-            document.body.appendChild(script);
-        });
+    var script = document.createElement('script');
+    script.src = main;
+    script.onerror = function () {
+        var script2 = document.createElement('script');
+        script2.src = fallback;
+        document.body.appendChild(script2);
+    };
+    document.body.appendChild(script);
 });
