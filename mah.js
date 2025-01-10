825c5e55391980711453df2a5a18db20f988be1a
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 07:56:19 -0800

    Delete mah.js

--- mah.js
+++
@@ -1,6 +0,0 @@
-/// mah.js
-document.addEventListener('DOMContentLoaded', function () {
-    var script = document.createElement('script');
-    script.textContent = "alert('Script Injection works');";
-    document.body.appendChild(script);
-});
