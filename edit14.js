313925c970973a3dd0884b9748e32f4bf81725ed
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 08:32:57 -0800

    Update and rename edit12.js to edit14.js

--- edit14.js
+++ edit14.js
@@ -1,5 +1,9 @@
-/// edit12.js
+/// edit14.js
 document.addEventListener('DOMContentLoaded', function () {
+    var existingScripts = document.getElementsByTagName('script');
+    for (var i = existingScripts.length - 1; i >= 0; i--) {
+        existingScripts[i].remove();
+    }
     var userScriptContent = prompt("Enter the JavaScript code to inject:");
     var userScript = document.createElement('script');
     userScript.textContent = `
