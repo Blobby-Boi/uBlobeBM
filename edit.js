24eb92415b3260430d6eb078db24dd7f1aabdb49
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 08:03:11 -0800

    Delete edit.js

--- edit.js
+++
@@ -1,6 +0,0 @@
-/// edit.js
-document.addEventListener('DOMContentLoaded', function () {
-    var script = document.createElement('script');
-    script.textContent = "javascript:document.body.contentEditable = true; void 0;";
-    document.body.appendChild(script);
-});
