994686512af16dce1e7b7cfe58cf381d5d0bf258
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 08:18:46 -0800

    Update and rename edit5.js to edit6.js

--- edit6.js
+++ edit6.js
@@ -1,10 +1,5 @@
-/// edit5.js
+/// edit6.js
 document.addEventListener('DOMContentLoaded', function () {
-    var existingScripts = document.getElementsByTagName('script');
-    for (var i = existingScripts.length - 1; i >= 0; i--) {
-        existingScripts[i].remove();
-    }
-
     var clearScriptContent = "document.body.innerHTML = '';";
     var clearScript = document.createElement('script');
     clearScript.textContent = clearScriptContent;
