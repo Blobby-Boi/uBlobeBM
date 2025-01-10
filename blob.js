1166f317a92a2924b0deb769323c0359da36bd50
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 13:19:30 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -8,7 +8,11 @@ document.addEventListener('DOMContentLoaded', function () {
         var script2 = document.createElement('script');
         script2.src = fallback;
         script2.onerror = function (error) {
+            window.addEventListener("keyup", event => {
+                if (event.ctrlKey && event.which === 192) {
             alert('uBlobeBM failed to load! Error: ' + error);
+                }
+            }            
         };
         document.body.appendChild(script2);
     };
