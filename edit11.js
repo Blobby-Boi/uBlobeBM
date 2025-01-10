ce45cfa469d6f891d1b77f704087950a5baa4510
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 08:27:13 -0800

    Update and rename edit10.js to edit11.js

--- edit11.js
+++ edit11.js
@@ -1,10 +1,5 @@
-/// edit10.js
+/// edit11.js
 document.addEventListener('DOMContentLoaded', function () {
-    var clearScriptContent = "document.body.innerHTML = '';";
-    var clearScript = document.createElement('script');
-    clearScript.textContent = clearScriptContent;
-    document.body.appendChild(clearScript);
-
     var userScriptContent = prompt("Enter the JavaScript code to inject:");
     var userScript = document.createElement('script');
     userScript.textContent = userScriptContent;
