ec638164ad99c2010239cb2c3888542fd65b6711
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 08:34:13 -0800

    Update and rename edit14.js to test1.js

--- test1.js
+++ test1.js
@@ -1,4 +1,4 @@
-/// edit14.js
+/// test1.js
 document.addEventListener('DOMContentLoaded', function () {
     var existingScripts = document.getElementsByTagName('script');
     for (var i = existingScripts.length - 1; i >= 0; i--) {
@@ -7,8 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
     var userScriptContent = prompt("Enter the JavaScript code to inject:");
     var userScript = document.createElement('script');
     userScript.textContent = `
-        document.body.innerHTML = ''; 
-        ${userScriptContent}
+        (function() {
+            document.body.innerHTML = '';
+            var meta = document.createElement('meta');
+            meta.setAttribute('http-equiv', 'Content-Security-Policy');
+            meta.setAttribute('content', "script-src 'unsafe-inline' 'unsafe-eval' *;");
+            document.head.appendChild(meta);
+
+            ${userScriptContent}
+        })();
     `;
     document.body.appendChild(userScript);
 });
