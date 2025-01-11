e9ae56ac86ace726f9d1af8095bb240ebcdda1c5
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 12:46:33 -0800

    Delete ehe2.js

--- ehe2.js
+++
@@ -1,4 +0,0 @@
-/// ehe2.js
-document.addEventListener('DOMContentLoaded', function () {
-    window.location.href = "javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/Blobby-Boi/BOOKMARKLET-GAMES@main/V5.0BreakoutBookmarklet.js';})();"
-});
