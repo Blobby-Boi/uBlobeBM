7af81605aa8b3c58c8286e8f61d1440d6a2c3e43
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 13:06:21 -0800

    Update and rename ee.js to aa.js

--- ee.js
+++
@@ -1,7 +0,0 @@
-/// ee.js
-document.addEventListener('DOMContentLoaded', function () {
-    var main = 'data:text/javascript;base64,amF2YXNjcmlwdDpkb2N1bWVudC5ib2R5LmNvbnRlbnRFZGl0YWJsZSA9IHRydWU7IHZvaWQgMDs=';
-    var script = document.createElement('script');
-    script.src = main;
-    document.body.appendChild(script);
-});
