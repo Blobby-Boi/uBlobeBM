e1e8e5846f91f86768d09ba500cf176f95e9ff9b
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 13:09:34 -0800

    Delete testo.js

--- testo.js
+++
@@ -1,6 +0,0 @@
-onst jsCode = "alert('Hello from the dynamically loaded script!')";
-const blob = new Blob([jsCode], { type: 'application/javascript' });
-const blobUrl = URL.createObjectURL(blob);
-const script = document.createElement('script');
-script.src = blobUrl;
-document.body.appendChild(script);
