e785cd540863e03e5e54309ab5f3d2163b78a86b
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 13:14:36 -0800

    Update and rename testt.js to jaja.js

--- jaja.js
+++ jaja.js
@@ -1,6 +1,6 @@
-/// testt.js
+/// jaja.js
 document.addEventListener('DOMContentLoaded', function () {
-    const jsCode = "alert('Hello from the dynamically loaded script!')";
+const jsCode = prompt("Please enter your JavaScript code:");
 const blob = new Blob([jsCode], { type: 'application/javascript' });
 const blobUrl = URL.createObjectURL(blob);
 const script = document.createElement('script');
