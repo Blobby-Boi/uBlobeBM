2ada5e3056c8b1b445e8d3379f00e925819e46f3
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 12:46:27 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -2,24 +2,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     var main = 'https://blobby-boi.github.io/uBlobeBM/main.js';
     var fallback = 'https://cdn.jsdelivr.net/gh/Blobby-Boi/uBlobeBM@657c7a8/main.js';
-    var fallback2 = 'https://google.com';
-    
     var script = document.createElement('script');
     script.src = main;
     script.onerror = function () {
         var script2 = document.createElement('script');
         script2.src = fallback;
         script2.onerror = function () {
-            var script3 = document.createElement('script');
-            script3.src = fallback2;
-            script3.onerror = function () {
-                window.addEventListener('keydown', function (event) {
-                    if (event.ctrlKey && event.which === 192) {
-                        alert('uBlobeBM failed to load on this page!');
-                    }
-                });
-            };
-            document.body.appendChild(script3);
+            window.addEventListener('keydown', function (event) {
+                if (event.ctrlKey && event.which === 192) {
+                    alert('uBlobeBM failed to load on this page!');
+                }
+            });
         };
         document.body.appendChild(script2);
     };
