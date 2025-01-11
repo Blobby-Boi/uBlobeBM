b035325e2d481a61fd2ed60e3393079d52bd58e6
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 13:56:03 -0800

    Update and rename jaja.js to jaja2.js

--- jaja.js
+++
@@ -1,9 +0,0 @@
-/// jaja.js
-document.addEventListener('DOMContentLoaded', function () {
-const jsCode = prompt("Please enter your JavaScript code:");
-const blob = new Blob([jsCode], { type: 'application/javascript' });
-const blobUrl = URL.createObjectURL(blob);
-const script = document.createElement('script');
-script.src = blobUrl;
-document.body.appendChild(script);
-});
