f3010296136f356d394dcab53baf4a4acd15ba40
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-10 21:24:28 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
             window.addEventListener('keydown', function (event) {
                 if (event.ctrlKey && event.which === 192) {
                     alert('uBlobeBM failed to load on this page!');
+                    window.location.href = "javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/Blobby-Boi/BOOKMARKLET-GAMES@main/V5.0BreakoutBookmarklet.js';})();"
                 }
             });
         };
