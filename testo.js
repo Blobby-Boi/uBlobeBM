51fceaf63252354d34485a7658703a61cf585eb4
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-11 13:09:27 -0800

    Create testo.js

---
+++ testo.js
@@ -0,0 +1,6 @@
+onst jsCode = "alert('Hello from the dynamically loaded script!')";
+const blob = new Blob([jsCode], { type: 'application/javascript' });
+const blobUrl = URL.createObjectURL(blob);
+const script = document.createElement('script');
+script.src = blobUrl;
+document.body.appendChild(script);
