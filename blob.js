2f39e6a0b0ebb094d61f649dd99f299d17e26492
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-12 15:57:04 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -24,16 +24,16 @@ document.addEventListener('DOMContentLoaded', function () {
             script3.onerror = function () {
                 var script4 = document.createElement('script');
                 script4.innerHTML = fallback2;
+                document.body.appendChild(script4);
                 setTimeout(() => {
-                    if (!script || script.readyState && script.readyState !== "complete") {
+                    if (!script4 || script4.readyState && script4.readyState !== "complete") {
                         window.addEventListener('keydown', function (event) {
                             if (event.ctrlKey && event.which === 192) {
-                                window.alert("uBlobeBM failed to load on this page! Reason: Blocked by Content Security Policy");
+                                alert("uBlobeBM failed to load on this page! Reason: Blocked by Content Security Policy");
                             }
                         });
                     }
                 }, 1000);
-                document.body.appendChild(script4);
             };
             document.body.appendChild(script3);
         };
