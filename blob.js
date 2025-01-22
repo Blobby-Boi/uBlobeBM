514cc4f05a0c373b6fe49caf7f0fb830b8c66fac
Blobby Boi <131911782+Blobby-Boi@users.noreply.github.com>
2025-01-21 18:03:51 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,7 +1,7 @@
 /// blob.js
 var backupAlert = window.alert;
 document.addEventListener('DOMContentLoaded', function () {
-    if (window.location.hostname.endsWith('.google.com') && window.location.hostname !== 'www.google.com' || window.location.hostname == 'www.youtube.com') {
+    if ((window.location.hostname.endsWith('.google.com') && window.location.hostname !== 'www.google.com') || window.location.hostname == 'www.youtube.com') {
         window.addEventListener('keydown', function (event) {
             if (event.ctrlKey && event.which === 192) {
                 window.alert("uBlobeBM failed to load on this page! Error: Google Subdomain");
